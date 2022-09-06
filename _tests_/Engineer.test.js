4882beeebe07fa12e95642d869b33f48a9066030
Jorge Garit <jorgegarit@gmail.com>
2022-09-06 00:44:41 -0400

    added test fail engineer name, email, id, and github and made sure it failed

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,16 @@
+const Engineer = require('../lib/Engineer');
+
+// Will test new container that includes employye inherited information plus git hub username
+
+test("creates a new engineer", () => {
+    const engineer = new Engineer('Jorge', 92593, 'jorgegarit@gmail.com', "jorgegarit");
+
+    // expected engineer name
+    expect(engineer.name).toEqual(expect.any(String));
+    // expected engineer id
+    expect(engineer.id).toEqual(expect.any(Number));
+    // expected engineer email
+    expect(engineer.email).toEqual(expect.any(String));
+    // expected engineer github username
+    expect(engineer.github).toEqual(expect.any(String));
+})
