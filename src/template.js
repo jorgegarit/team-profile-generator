7fa4f3a58602b1ddc3de06d05f7f0fede25b3752
Jorge Garit <jorgegarit@gmail.com>
2022-09-06 19:58:49 -0400

    template for manager title done

---
+++ src/template.js
@@ -0,0 +1,14 @@
+//  html for the manager 
+const createManager = manager => {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card emp-card">
+            <div class="card-header bg-dark text-info">
+                <h2 class="card-title">${manager.getName()}</h2>
+                <h3 class="card-title">${manager.getRole()}</h3>
+            </div>
+
+    `
+    }
+    
+
