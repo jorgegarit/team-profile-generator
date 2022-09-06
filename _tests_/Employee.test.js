8aa746690a300da8af3aa824fb89ed26a82421d2
Jorge Garit <jorgegarit@gmail.com>
2022-09-06 00:09:53 -0400

    first test for created employee failed

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,15 @@
+//  testing with the Employee constructor
+
+const Employee = require('../lib/Employee');
+
+// Create new Employee
+test('creates a new employee', () => {
+    const employee = new Employee('Jorge', '092593', 'jorgegarit@gmail.com')
+
+    // expected employee name
+    expect(employee.name).toEqual(expect.any(String));
+    // expected employee id
+    expect(employee.id).toEqual(expect.any(Number));
+    // expected employee email
+    expect(employee,email).toEqual(expect.any(String));
+})
