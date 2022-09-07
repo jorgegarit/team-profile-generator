51963121ae1baf9c38e0dd972df271bcba002d69
Jorge Garit <jorgegarit@gmail.com>
2022-09-06 20:25:37 -0400

    added the engineer html section copying from manager section

--- src/template.js
+++ src/template.js
@@ -16,5 +16,24 @@ const createManager = manager => {
     </div>
     `;
 }
+
+// html for the engineer
+const createEngineer = engineer => {
+    return `
+    <div class="col-4 mt-4">
+    <div class="card emp-card">
+        <div class="card-header bg-dark text-info">
+            <h2 class="card-title">${engineer.getName()}</h2>
+            <h3 class="card-title">${engineer.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <p class="empId>Id: ${engineer.id}</p>
+            <p class="empEmail>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+            <p class="empGitHub">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</p>
+        </div>
+    </div>
+</div>
+    `;
+}
     
 
